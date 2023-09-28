ca2cb50a72222b470b2a38b082493f0005a9c841
Sarah Gushef <sarahgushef@gmail.com>
2023-09-28 19:31:56 +0700

    Adding cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import "dotenv/config.js";
 import express from "express";
+import cors from "cors";
 import PatientRoute from "./routes/PatientRoute.js";
 import BookingRoute from "./routes/BookingRoute.js";
 
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // Route
 app.get("/", (req, res) => {
